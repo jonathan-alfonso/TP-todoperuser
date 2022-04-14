49d90ad30b6fa73279394779a23a319126725f95
Jonathan Alfonso <alfonso.jonathan@protonmail.com>
2022-04-14 11:47:30 +0200

    Commit précédent + fichiers qui n'ont pas été commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './src/footer/footer.component';
 import { HeaderComponent } from './src/header/header.component';
 import { TableComponent } from './src/table/table.component';
+import { TodosComponent } from './src/todos/todos.component';
 
 
 @NgModule({
@@ -14,12 +16,14 @@ import { TableComponent } from './src/table/table.component';
     AppComponent,
     HeaderComponent,
     TableComponent,
-    FooterComponent
+    FooterComponent,
+    TodosComponent
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
