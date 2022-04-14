aa4345c1738a880f8ab3d8dea948ea319d9026a3
Jonathan Alfonso <alfonso.jonathan@protonmail.com>
2022-04-14 11:46:43 +0200

    Ajout des actions

--- src/app/src/table/table.component.ts
+++ src/app/src/table/table.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Users } from 'src/app/shared/models/users';
 import { TableService } from 'src/app/shared/services/table.service';
+import { faEraser, faUserSlash, faUsersViewfinder } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-table',
@@ -21,5 +23,9 @@ export class TableComponent implements OnInit {
         this.users = users;
       });
   }
+
+  faUpdate = faEraser;
+  faDelete = faUserSlash;
+  faTodos = faUsersViewfinder;
 
 }
